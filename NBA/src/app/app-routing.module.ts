752b2d4d8183b483c4cec9fabcf58cf18a647e45
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2024-01-03 17:25:31 +0100

    ultimato estetica home, classifica

--- NBA/src/app/app-routing.module.ts
+++ NBA/src/app/app-routing.module.ts
@@ -39,7 +39,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  // imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

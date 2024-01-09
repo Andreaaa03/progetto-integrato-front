2fd7da09f8b9c7a58356d6e4de60a69d87d1a157
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2024-01-09 20:01:35 +0100

    componente articolo e card articolo

--- NBA/src/app/components/articolo/articolo.component.ts
+++ NBA/src/app/components/articolo/articolo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-articolo',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./articolo.component.css']
 })
 export class ArticoloComponent {
-
+  @Input() extended: boolean = true;
 }

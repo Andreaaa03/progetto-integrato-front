43ed20f58456114115813ef77439938d25936e4b
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2024-01-10 18:05:19 +0100

    finito tabellino

--- NBA/src/app/views/partita-page/partita-page.component.ts
+++ NBA/src/app/views/partita-page/partita-page.component.ts
@@ -9,6 +9,16 @@ export class PartitaPageComponent {
   quarti:number[]=[1,2,3,4];
   tabsSelected: string ="statics";
   staticsToShow: number[]=[1,2,3,4,5,6,7,7];  
-  lato1=10;
-  lato2=50;
+  lato1=2;
+  lato2=2;
+
+  functionCalculatePercentage(lato1: number, lato2: number, whichLato: boolean): number {
+  const sum = lato1 + lato2;
+  const percentage1 = Math.round((lato1 / sum) * 100);
+  const percentage2 = Math.round((lato2 / sum) * 100);
+
+  console.log(percentage1, percentage2);
+
+  return whichLato ? percentage1 : percentage2;
+}
 }

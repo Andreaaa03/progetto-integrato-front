7432f21571a55f3250d425d19abf29dfc4afb7fd
Lorenzo Garnero <106029233+GarneroLorenzo15@users.noreply.github.com>
2024-01-19 20:01:39 +0100

    modal login

--- NBA/src/app/components/login/login.component.ts
+++ NBA/src/app/components/login/login.component.ts
@@ -1,20 +1,42 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent{
 
   modal: boolean = false;
+
+  registrati: boolean = true;
+
+  registratiView(){
+    this.registrati = !this.registrati;
+  }
 
   utente: string = 'admin';
   password: string = 'admin';
   errorMessage: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute, private location: Location) { }
+
+  confrontUrl(){
+
+    const urlAttuale = this.router.url;
+
+    const urlDesiderato = 'localhost:4200/profilo'
+
+    if(urlAttuale === urlDesiderato){
+      location.reload();
+    }else{
+
+    }
+  }
+
+
 
   login(): void {
     if (this.utente === 'admin' && this.password === 'admin') {

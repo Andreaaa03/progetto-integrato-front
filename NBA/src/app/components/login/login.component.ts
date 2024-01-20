819c5476ea301b5b0cf60ee895809c924a20a4bc
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2024-01-20 12:45:46 +0100

    corretto sovrapposizioni login

--- NBA/src/app/components/login/login.component.ts
+++ NBA/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -7,9 +7,10 @@ import { Location } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginPageComponent{
+export class LoginPageComponent implements OnInit{
 
-  modal: boolean = false;
+  ngOnInit(): void {
+  }
 
   registrati: boolean = true;
 
@@ -23,24 +24,11 @@ export class LoginPageComponent{
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location) { }
 
-  confrontUrl(){
-
-    const urlAttuale = this.router.url;
-
-    const urlDesiderato = 'localhost:4200/profilo'
-
-    if(urlAttuale === urlDesiderato){
-      location.reload();
-    }else{
-
-    }
-  }
 
 
 
   login(): void {
     if (this.utente === 'admin' && this.password === 'admin') {
-      console.log('ciao');
       this.router.navigate(['/profilo']);
     } else {
       this.errorMessage = 'Credenziali non valide. Riprova.';
--- NBA/src/app/components/navbar/navbar.component.ts
+++ NBA/src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import * as dayjs from 'dayjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent{
+export class NavbarComponent {
+  constructor(private location: Location) { }
   dateToday = dayjs().format('YYYY-MM-DD');
   isMobileMenuOpen: boolean = false;
-  modal:boolean=false;
+  modal: boolean = false;
   toggleMenu(isMobileMenuOpen: boolean) {
     this.isMobileMenuOpen = !isMobileMenuOpen;
+  }
+  confrontUrl() {
+    const urlAttuale = this.location.path().split("/")[1]
+    const urlDesiderato = 'profilo';
+    if (urlAttuale === urlDesiderato) {
+      this.modal = false
+    } else {
+      this.modal = true;
+    }
   }
 }

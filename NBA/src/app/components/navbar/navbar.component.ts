8a001b8aec5bb1c46322a1299f3b8b64fa899a00
Cavalca Andrea <92914394+Andreaaa03@users.noreply.github.com>
2024-01-21 12:19:40 +0100

    corretto login e tolto il componente login
    spostato il tutto dentro navbar

--- NBA/src/app/components/navbar/navbar.component.ts
+++ NBA/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as dayjs from 'dayjs';
 import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,7 @@ import { Location } from '@angular/common';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(private location: Location) { }
+  constructor(private location: Location, private router: Router, private route: ActivatedRoute,) { }
   dateToday = dayjs().format('YYYY-MM-DD');
   isMobileMenuOpen: boolean = false;
   modal: boolean = true;
@@ -22,6 +23,34 @@ export class NavbarComponent {
       this.modal = false
     } else {
       this.modal = true;
+    }
+  }
+
+
+  //login e registrati
+  registrati: boolean = false;
+
+  registratiView() {
+    this.registrati = !this.registrati;
+  }
+
+  utente: string = 'admin';
+  password: string = 'admin';
+  errorMessage: string = '';
+
+
+  selectedOption: string = '';
+
+  onSelectionChange() {
+    console.log('Opzione selezionata:', this.selectedOption);
+  }
+
+
+  login(): void {
+    if (this.utente === 'admin' && this.password === 'admin') {
+      this.router.navigate(['/profilo']);
+    } else {
+      this.errorMessage = 'Credenziali non valide. Riprova.';
     }
   }
 }
